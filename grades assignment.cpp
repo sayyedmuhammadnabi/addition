62490bbca5b7cb2098d1234c52c24f84f4b12ac4
sayyedmuhammadnabi <nabi32251@gmail.com>
2025-11-29 07:47:16 -0800

    Add files via upload

---
+++ grades assignment.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int marks;
+	cout << "enter your marks: ";
+	cin >> marks;
+	
+	if(marks>=85 && marks <= 100)
+	{
+	cout <<"A+";	
+	}
+	 else if(marks>=80 && marks <=84)
+	 {
+	 	cout<<"A-"<<endl;
+	 }
+	 else if(marks>=75 && marks<=79)
+	 {
+	 	cout<<"B+";
+	 }
+	 else if(marks>=70 &&marks <=74)
+	 {
+	 	cout <<"B";
+	 }
+	 else if(marks>=65 &&marks <=69)
+	 {
+	 	cout<<"B-";
+	 }
+	 else if(marks>=61 &&marks <=64)
+	 {
+	 	cout<<"C+";
+	 }
+	 else if(marks>=58 &&marks <=60)
+	 {
+	 	cout<<"C";
+	 }
+	 else if(marks>=55 &&marks <=57)
+	 {
+	 	cout<<"C-";
+	 }
+	 else if(marks>=50 &&marks <=54)
+	 {
+	 	cout<<"D";
+	 }
+	 else if(marks>=0 &&marks <=49)
+	 {
+	 	cout<<"fail";
+	 }
+	 else if(marks>=61 &&marks <64)
+	 {
+	 	cout<<"C+";
+	 }
+	 else {
+	 	cout << "Please enter valid marks";
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
