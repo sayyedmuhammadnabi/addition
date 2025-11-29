9e876806c71db2d1b1df39ac23872da0440d7d9e
sayyedmuhammadnabi <nabi32251@gmail.com>
2025-11-29 07:41:41 -0800

    Add files via upload

---
+++ operators assignment1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int main()
+{                                 //Arithmatic operators = +,-,*,/; //
+//int num1 =5;
+//int num2=10;
+//int sum= num1+num2;
+//cout << "addition of num1 and num2 is:"<<sum<<endl;
+
+
+                              //Camparisonal operators is ==,<,<=,>,>=,!=
+//cout<<(10>5)<<endl;
+//cout<<(20<30)<<endl;
+//cout<<(45<=45)<<endl;
+//cout<<(50>=50)<<endl;
+//cout<<("ball"=="ball")<<endl;
+//cout<<(45!=50)<<endl;
+//cout<<("ink"!="int")<<endl;
+//cout<<('A'=='B')<<endl;
+                                        //Logical operators are &&,||,!
+//AND && :
+//int age=17;
+//int marks=562;
+ //if (age>=18&&marks<=562){
+ 	//cout<<"programs execute"<<endl;
+// }//else {
+ //cout<<"program not execute"<<endl;
+ 
+                                                  //OR ||:
+
+//  int age=18;
+// int required=5;
+// 
+// if (age>=18||required==4) {
+// 	cout<<"you can enter"<<endl;
+// }else
+// { cout<<"you can not enter"<<endl;
+// }
+                                                   //if and else and grade
+int marks = 45;
+
+if (marks >= 80)
+{
+	cout << "A+";
+}
+else if (marks > 70)
+{
+	cout << "B";
+}
+else if(marks > 50) {
+	cout << "C";
+}
+
+else 
+{
+	cout << "Fail";
+}
+
+
+
+}
+
